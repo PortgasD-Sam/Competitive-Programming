c7f76ae902853abaea63b908230642816becd86c
Sambit Ekka <ekka.sambit@gmail.com>
2019-01-11 21:34:03 +0530

    Update Frog Jumping - 1077A - Codeforces.cpp

--- Implementation/Frog Jumping - 1077A - Codeforces.cpp
+++ Implementation/Frog Jumping - 1077A - Codeforces.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+using ll = long long;
 
-int t, a, b, k;
-
-int main() {
-  scanf("%d", &t);
-  while(t-- != 0) {
-    scanf("%d %d %d", &a, &b, &k);
-
-    int aa = (k + 1) / 2;
-    int bb = k / 2;
-
-    long long res = 1ll * a * aa - 1ll * b * bb;
-    printf("%lld\n", res);
-  }
-
-  return 0;
+int main()
+{
+    ll t{},a{},b{},k{};
+    scanf("%lld",&t);
+    while(t--)
+    {
+        scanf("%lld%lld%lld",&a,&b,&k);
+        ll aa = (k+1)/2;
+        ll bb = k/2;
+        ll result = a*aa - b*bb;
+        printf("%lld\n",result);
+    }
+    return 0;
 }
