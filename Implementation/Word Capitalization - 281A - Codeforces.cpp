61904257e062d19a65a4f06ccfc89346a29e3b33
Sambit Ekka <ekka.sambit@gmail.com>
2019-01-16 17:01:14 +0530

    Add files via upload

---
+++ Implementation/Word Capitalization - 281A - Codeforces.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    string s;
+    cin>>s;
+    s[0] = toupper(s[0]);
+    cout<<s<<endl;
+    return 0;
+}
