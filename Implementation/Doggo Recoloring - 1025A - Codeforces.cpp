e4494803a1d53b7c199f6936dec4e0ecddfb9e17
Sambit Ekka <ekka.sambit@gmail.com>
2019-02-24 21:38:40 +0530

    Add files via upload

---
+++ Implementation/Doggo Recoloring - 1025A - Codeforces.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+string s;
+
+int main() {
+  cin >> n >> s;
+
+  if(n == 1) {
+    puts("Yes");
+    return 0;
+  }
+
+  int fr[26] = {0};
+  for(int i = 0; i < n; ++i)
+    ++fr[s[i] - 'a'];
+
+  for(int i = 0; i < 26; ++i)
+    if(fr[i] > 1) {
+      puts("Yes");
+      return 0;
+    }
+
+  puts("No");
+
+  return 0;
+}
