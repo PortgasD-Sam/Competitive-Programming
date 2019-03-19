595daabf6a8603c9144062366751543f30a88bd8
Sambit Ekka <ekka.sambit@gmail.com>
2019-03-19 22:19:52 +0530

    Add files via upload

--- Implementation/Football - 96A - Codeforces.cpp
+++ Implementation/Football - 96A - Codeforces.cpp
@@ -1,29 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
-#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define FORI(n) for(int i=0;i<n;i++)
-#define endl "\n"
 
-int main()
-{
-    IOS;
-    string s;
-    cin>>s;
-    s+="H";
-    int n = s.length(),c{1},mx{INT_MIN};
-    FORI(n-1)
-    {
-        if(s[i] == s[i+1])
-        c++;
-        else
-        {
-            mx = max(mx,c);
-            c = 1;
-        }
-    }
-    if(mx>=7)
-    cout<<"YES\n";
-    else
-    cout<<"NO\n";
-    return 0;
+int main() {
+	string s;
+	cin >> s;
+	s += ' ';
+	int count = 0;
+	
+	for(int i = 0; i < s.length(); i++) {
+		if(s[i] == s[i+1])
+			count++;
+		else
+			count = 0;
+			
+		if(count >= 6) {
+			cout << "YES" << endl;
+			return 0;
+		}
+	}
+	
+	cout << "NO" << endl;
+	
+	return 0;
 }
