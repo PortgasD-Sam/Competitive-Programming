6fa1ddb6e2a4b179a4de615c9dd08cd71e2138f8
Sambit Ekka <ekka.sambit@gmail.com>
2019-06-27 23:53:45 +0530

    Add files via upload

---
+++ Implementation/Moore's Law - 630B - Codeforces.cpp
@@ -0,0 +1,10 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main()
+    {
+    	int n,t;
+    	scanf("%d%d",&n,&t);
+    	double res = pow(1.000000011, t) * n;
+    	return !printf("%lf\n",res);
+    }
