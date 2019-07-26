f7da03b6dc9e857c903404debc0bb34dd9ddf623
Sambit Ekka <ekka.sambit@gmail.com>
2019-07-26 23:32:02 +0530

    Add files via upload

---
+++ Implementation/Calculating Function - 486A - Codeforces.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long n;
+
+int main() {
+  scanf("%lld", &n);
+  
+  if(n & 1)
+    printf("%lld\n", (n - 1) / 2 - n);
+  else
+    printf("%lld\n", n / 2);
+  
+  return 0;
+}
